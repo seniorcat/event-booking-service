2e4cdf4a4e0888f5d88b69ee542cb8b51b46a619
BearBQ <d.a.demkin@gmail.com>
2026-02-05 23:31:24 +0500

    мелкие правки для переноса на бук

--- internal/http/middleware/metrics.go
+++ internal/http/middleware/metrics.go
@@ -1,1 +1,50 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	requestsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "event_booking_http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "handler", "status"},
+	)
+
+	requestDuration = promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "event_booking_http_request_duration_seconds",
+			Help:    "HTTP request duration in seconds",
+			Buckets: []float64{0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10},
+		},
+		[]string{"method", "route", "status"},
+	)
+	errorsTotal = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "event_booking_http_errors_total",
+			Help: "Total http server errors (5xx)",
+		},
+		[]string{"method", "route", "status"},
+	)
+)
+
+type metricsResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *metricsResponseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func MetricsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	})
+}
